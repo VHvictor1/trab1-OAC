f6bda76c2d0ee3e2f4b21b995445c7aff4fb8184
VHvictor1 <vitorios20001@gmail.com>
2022-06-26 17:38:28 -0300

    finish the code documentation

--- memory.c
+++ memory.c
@@ -47,7 +47,10 @@ int32_t lbu(uint32_t address, int32_t kte)
     lastByte = lastByte & 0x000000ff;
     return lastByte;
 };
-
+// Criação da função storeWord utilizada para adicionar uma palavra no nosso array, porém temos de checar se o nosso
+// endereço é divisível por 4 poís a primeira posição de cada palavra é um número divisível por 4, assim escrevemos
+// na posição de endereço dividida por 4.
+// {linhas: 54-64}.
 void sw(uint32_t address, int32_t kte, int32_t dado)
 {
     if ((address + kte) % 4 == 0)
@@ -59,7 +62,11 @@ void sw(uint32_t address, int32_t kte, int32_t dado)
         printf("Endereço errado");
     }
 };
-
+// Criação da função storebit utilizada para adicionar um bit no nosso array, assim criamos um ponteiro para apontar o
+// local na memória e falamos que o endereço do nosso ponteiro vai ser o endereço da memória mais o address mais a 
+// constante (kte) assim falamos que o valor do pointer é igual ao nosso dado a ser armazenado.
+// na posição de endereço dividida por 4.
+// {linhas: 70-76}.
 void sb(uint32_t address, int32_t kte, int8_t dado)
 {
     int8_t *pointer;
@@ -67,8 +74,9 @@ void sb(uint32_t address, int32_t kte, int8_t dado)
     pointer = (pointer + (address + kte));
     *pointer = dado;
 }; 
-
-
+// Função principal onde possui nossas variáveis b0 - b11 (bit 0 - bit 11), recebe os valores do usuário e executa as
+// funções pedidas na ordem pedida.
+// {linhas: 80-116}.
 int main()
 {
     int8_t b0, b1, b2, b3, b4, b5, b6, b7;
@@ -85,19 +93,20 @@ int main()
     sb(0, 1, b1);
     sb(0, 2, b2);
     sb(0, 3, b3);
-    printf("%.08x\n", mem[0]);
     sb(4, 0, b4);
     sb(4, 1, b5);
     sb(4, 2, b6);
     sb(4, 3, b7);
-    printf("%.08x\n", mem[1]);
     sw(8, 0, b8);
-    printf("%.08x\n", mem[2]);
     sw(12, 0, b9);
-    printf("%.08x\n", mem[3]);
     sw(16, 0, b10);
-    printf("%.08x\n", mem[4]);
     sw(20, 0, b11);
+
+    printf("%.08x\n", mem[0]);
+    printf("%.08x\n", mem[1]);
+    printf("%.08x\n", mem[2]);
+    printf("%.08x\n", mem[3]);
+    printf("%.08x\n", mem[4]);
     printf("%.08x\n", mem[5]);
     printf("%.8x\n", lb(4, 0));
     printf("%.8x\n", lbu(4, 0));
@@ -105,4 +114,4 @@ int main()
     printf("%.8x\n", lw(16, 0));
     printf("%.8x\n", lw(20, 0));
 };
-
+// ©Código feito por Victor Hugo da Silva de Oliveira, não dever ser copiado, deve ser usado apenas para estudos.
